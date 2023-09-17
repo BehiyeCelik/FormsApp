a969a99e132401257435318f07905fce8c2bb3e6
BehiyeCelik <celik.behiye@outlook.com>
2023-09-18 00:11:16 +0200

    Get and Post form, Select Categories, View Model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormsApp.Controllers;
 
@@ -10,12 +11,36 @@ public class HomeController : Controller
     {
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string searchString, string category ) // parametremizi belirledik
     {
-        return View(Repository.Products);
-    }
+        var products = Repository.Products;     //Products bilgisini aldık 
 
-    public IActionResult Privacy()
+        if(!String.IsNullOrEmpty(searchString))   // parametre null değilse çalışması gerekiyor
+        {
+            ViewBag.SearchString = searchString; // bunun sayesinde aradığımız kelime arama yerinde yazılı olarak kalacak kaybolmayacak.Get Formu dersi için geçerli. 
+            products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList(); // her bir product bilgisini alacağız ve searchstring içinde varsa bir listeye aktarılsın ve bize geri dönsün ismin içinde küçük harf kullanırsın diye de Tolower koydum.
+        }
+        if(!String.IsNullOrEmpty(category) && category != "0")
+        {
+            products = products.Where(p => p.CategoryId == int.Parse(category)).ToList(); //Gelen her bir produckt kontrol edilir CatgoryId si yukarıdaki gönderilenle aynıysa, string olarak alıyoruz ama int yapmamız lazım çünkü sayı. bunu da product ın içine at. 
+        }
+
+        // ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category); //görünürde name var olacak ama arka tarafta categoryıd nin değeri dönecek. 
+        var model = new ProductViewModel{
+                Products=products,
+                Categories= Repository.Categories,
+                SelectedCategory= category
+        }; 
+                return View(model);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        ViewBag.Categories = Repository.Categories;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Product model)
     {
         return View();
     }
